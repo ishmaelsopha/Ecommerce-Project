1d61cd050a706f351385d5187f55e809333e06e1
ishmaelsopha <isoph4@gmail.com>
2022-08-05 02:48:07 -0500

    Last Check in before I turn in, pagination check

--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -9,6 +9,10 @@
         public List<Product> Products { get; set; } = new List<Product>();
         public string Message { get; set; } = "Loading products...";
 
+        public int CurrentPage { get; set; } = 1;
+        public int PageCount { get; set; } = 0;
+
+        public string LastSearchText { get; set; } = string.Empty;
         public ProductService(HttpClient http)
         {
             _http = http;
@@ -28,15 +32,25 @@
             if (result != null && result.Data != null)
                 Products = result.Data;
 
+            CurrentPage = 1;
+            PageCount = 0;
+
+            if (Products.Count == 0)
+                Message = "No products found";
             ProductsChanged.Invoke();
         }
 
-        public async Task SearchProducts(string searchText)
+        public async Task SearchProducts(string searchText,int page)
         {
             var result = await _http
-                .GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+                .GetFromJsonAsync<ServiceResponse<ProductSearchResult>>($"api/product/search/{searchText}/{page}");
             if (result != null && result.Data != null)
-                Products = result.Data;
+            {
+                Products = result.Data.Products;
+                CurrentPage = result.Data.CurrentPage;
+                PageCount = result.Data.Pages;
+            }
+                
             if (Products.Count == 0) Message = "No products found.";
             ProductsChanged?.Invoke();
         }
