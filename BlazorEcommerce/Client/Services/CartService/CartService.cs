8658ffb4dc8997d53a5c6269842e2db8de055a13
ishmaelsopha <isoph4@gmail.com>
2022-08-06 03:15:57 -0500

    Cart Removal/Add

--- BlazorEcommerce/Client/Services/CartService/CartService.cs
+++ BlazorEcommerce/Client/Services/CartService/CartService.cs
@@ -4,9 +4,12 @@ namespace BlazorEcommerce.Client.Services.CartService
     public class CartService : ICartService
     {
         private readonly ILocalStorageService _localStorage;
-        public CartService(ILocalStorageService localStorage)
+        private readonly HttpClient _http;
+
+        public CartService(ILocalStorageService localStorage, HttpClient http)
         {
             _localStorage = localStorage;
+            _http = http;
         }        
         public event Action OnChange;
 
@@ -20,6 +23,7 @@ namespace BlazorEcommerce.Client.Services.CartService
             cart.Add(cartItem);
 
             await _localStorage.SetItemAsync("cart", cart);
+            OnChange.Invoke();
         }
 
         public async Task<List<CartItem>> GetCartItems()
@@ -31,6 +35,32 @@ namespace BlazorEcommerce.Client.Services.CartService
             }
 
             return cart;
+        }
+
+        public async Task<List<CartProductResponse>> GetCartProducts()
+        {
+            var cartItems = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+            var response = await _http.PostAsJsonAsync("api/cart/products", cartItems);
+            var cartProducts = 
+                await response.Content.ReadFromJsonAsync<ServiceResponse<List<CartProductResponse>>>();
+            return cartProducts.Data;
+        }
+
+        public async Task RemoveProductFromCart(int productId, int productTypeId)
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+            if (cart == null)
+            {
+                return;
+            }
+
+            var cartItem = cart.Find(x => x.ProductId == productId && x.ProductTypeId == productTypeId);
+            if(cartItem != null)
+            { 
+                cart.Remove(cartItem);
+                await _localStorage.SetItemAsync("cart", cart);
+                OnChange.Invoke();
+            }
         }
     }
 }
