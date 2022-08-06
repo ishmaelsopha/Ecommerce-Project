fd835bc20bfa38fdcce9438c060c4f78ed85c216
ishmaelsopha <isoph4@gmail.com>
2022-08-06 03:59:13 -0500

    Remove Cart, and Quantity

--- BlazorEcommerce/Client/Services/CartService/CartService.cs
+++ BlazorEcommerce/Client/Services/CartService/CartService.cs
@@ -20,8 +20,16 @@ namespace BlazorEcommerce.Client.Services.CartService
             {
                 cart = new List<CartItem>();
             }
-            cart.Add(cartItem);
 
+            var sameItem = cart.Find(x => x.ProductId == cartItem.ProductId && x.ProductTypeId == cartItem.ProductTypeId);
+            if (sameItem == null)
+            {
+                cart.Add(cartItem);
+            }
+            else
+            {
+                sameItem.Quantity += cartItem.Quantity;
+            }
             await _localStorage.SetItemAsync("cart", cart);
             OnChange.Invoke();
         }
@@ -60,6 +68,22 @@ namespace BlazorEcommerce.Client.Services.CartService
                 cart.Remove(cartItem);
                 await _localStorage.SetItemAsync("cart", cart);
                 OnChange.Invoke();
+            }
+        }
+
+        public async Task UpdateQuantity(CartProductResponse product)
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+            if (cart == null)
+            {
+                return;
+            }
+
+            var cartItem = cart.Find(x => x.ProductId == product.ProductID && x.ProductTypeId == product.ProductTypeId);
+            if (cartItem != null)
+            {
+                cartItem.Quantity = product.Quantity;
+                await _localStorage.SetItemAsync("cart", cart);    
             }
         }
     }
--- BlazorEcommerce/Shared/CartProductResponse.cs
+++ BlazorEcommerce/Shared/CartProductResponse.cs
@@ -18,5 +18,7 @@ namespace BlazorEcommerce.Shared
         public string ImageUrl { get; set; } = string.Empty;
 
         public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
     }
 }
